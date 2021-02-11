f1686a8f3e71abc29462e773faea65df5adb7c9d
BH571 <muppanenibharath571@gmail.com>
2021-02-11 11:46:50 +0530

    day4 - css, resourcemodel

--- README.md
+++ README.md
@@ -1,2 +1,5 @@
 # UI5_Learning
 This is for learning purpose
+
+
+# for friends - Delete kiya to markayega 😁.
---
+++ day4/controller/View1.controller.js
@@ -0,0 +1,19 @@
+sap.ui.define([
+	"sap/ui/core/mvc/Controller"
+], function(Controller) {
+
+	return Controller.extend("day4.controller.View1", {
+		onInit: function(){
+			debugger;
+		},
+		onBeforeRendering: function(){
+			debugger;
+		},
+		onAfterRendering: function(){
+			debugger;
+		},
+		onExit: function(){
+			debugger;
+		}
+	});
+});
