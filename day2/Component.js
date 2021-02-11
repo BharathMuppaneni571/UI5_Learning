7b130d4e4b0597b5ffe205e7e846574b8711a3dd
BH571 <muppanenibharath571@gmail.com>
2021-02-11 10:49:20 +0530

    Day1, Day2, Day3 added.

---
+++ day2/Component.js
@@ -0,0 +1,13 @@
+sap.ui.define([
+	"sap/ui/core/UIComponent"
+], function(UIComponent) {
+	"use strict";
+
+	return UIComponent.extend("day2.Component", {
+		init: function() {
+			debugger;
+			// call the base component's init function
+			UIComponent.prototype.init.apply(this, arguments);
+		}
+	});
+});
---
+++ day3/Component.js
@@ -0,0 +1,16 @@
+sap.ui.define([
+	"sap/ui/core/UIComponent"
+], function(UIComponent) {
+
+	return UIComponent.extend("day3.Component", {
+		metadata: {
+			manifest: "json"
+		},
+
+		init: function() {
+			debugger;
+			// call the base component's init function
+			UIComponent.prototype.init.apply(this, arguments);
+		}
+	});
+});
