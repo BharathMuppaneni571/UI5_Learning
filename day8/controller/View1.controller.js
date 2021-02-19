0d6671a48bf7798dbb70128462104f4ae7718c2f
BH571 <muppanenibharath571@gmail.com>
2021-02-19 18:34:00 +0530

    day 8- sap.m.Table control using Northwind OData service

---
+++ day8/controller/View1.controller.js
@@ -0,0 +1,13 @@
+sap.ui.define([
+	"sap/ui/core/mvc/Controller"  //,
+	// "sap/ui/model/odata/ODataModel"
+], function(Controller/*,ODataModel*/) {
+	"use strict";
+	return Controller.extend("day8.controller.View1", {
+		onInit: function() {
+			// var oModel = new ODataModel("https://services.odata.org/V2/Northwind/Northwind.svc");
+			// this.getView().setModel(oModel);
+			
+		}
+	});
+});
