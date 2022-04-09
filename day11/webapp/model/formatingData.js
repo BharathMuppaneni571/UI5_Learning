c4d5bfef746d2f2cdab88909a9b2f63d13771e50
Bharath Muppaneni <42097066+BharathMuppaneni571@users.noreply.github.com>
2022-04-09 20:45:11 +0530

    errorMessageHandling

---
+++ day11/webapp/model/formatingData.js
@@ -0,0 +1,19 @@
+// var data = [];
+// res.forEach((item) => {
+//     switch (item.Type) {
+//         var tmp ={};
+//         case "E":
+//             tmp.type = sap.ui.core.MessageType.Error;
+//             tmp.message = item.Message;
+//             break;
+//         case "S":
+//             tmp.type = sap.ui.core.MessageType.Success;
+//             tmp.message = item.Message;
+//             break;
+//         default:
+//             tmp.type = sap.ui.core.MessageType.None;
+//             tmp.message = item.Message;
+//             break;
+//     }
+// data.push(tmp);
+// });
