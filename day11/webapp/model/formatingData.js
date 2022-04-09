9635d3155de5710d6a7bb572d6dfaa24108ad05c
Bharath Muppaneni <42097066+BharathMuppaneni571@users.noreply.github.com>
2022-04-09 20:58:25 +0530

    small bug fix.

--- day11/webapp/model/formatingData.js
+++ day11/webapp/model/formatingData.js
@@ -1,19 +1,19 @@
-// var data = [];
-// res.forEach((item) => {
-//     switch (item.Type) {
-//         var tmp ={};
-//         case "E":
-//             tmp.type = sap.ui.core.MessageType.Error;
-//             tmp.message = item.Message;
-//             break;
-//         case "S":
-//             tmp.type = sap.ui.core.MessageType.Success;
-//             tmp.message = item.Message;
-//             break;
-//         default:
-//             tmp.type = sap.ui.core.MessageType.None;
-//             tmp.message = item.Message;
-//             break;
-//     }
-// data.push(tmp);
-// });
+var data = [];
+res.forEach((item) => {
+    var tmp ={};
+    switch (item.Type) {
+        case "E":
+            tmp.type = sap.ui.core.MessageType.Error;
+            tmp.message = item.Message;
+            break;
+        case "S":
+            tmp.type = sap.ui.core.MessageType.Success;
+            tmp.message = item.Message;
+            break;
+        default:
+            tmp.type = sap.ui.core.MessageType.None;
+            tmp.message = item.Message;
+            break;
+    }
+data.push(tmp);
+});
